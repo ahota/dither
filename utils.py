da5e511637b1b73aad5ceeb9f848e474ea6b77d0
Alok Hota <ahota@vols.utk.edu>
2018-06-20 09:26:50 -0500

    faster euclidean distance calculation

--- utils.py
+++ utils.py
@@ -28,9 +28,7 @@ def closest_palette_color(value, palette_name, bit_depth=1):
     min_dist = 10000.
     ci_use = -1
     for ci, color in enumerate(palette.palettes[palette_name]):
-        pr, pg, pb = color
-        vr, vg, vb = value
-        dist = math.sqrt((vr-pr)*(vr-pr)+(vg-pg)*(vg-pg)+(vb-pb)*(vb-pb))
+        dist = numpy.linalg.norm(value-color)
 
         if DEBUGMODE:
             print '\tcolor = {}'.format(color)
