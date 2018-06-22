3d4ce7a8a0de914796c2b152014e604021ff229d
Alok Hota <ahota@vols.utk.edu>
2018-06-22 10:44:36 -0500

    added 8x8 cluster dot dithering

--- ordered_dithering.py
+++ ordered_dithering.py
@@ -34,6 +34,17 @@ _clusterdot4x4map = 1./15. * numpy.array([
     [15, 10,  9, 14]
 ])
 
+_clusterdot8x8map = 1./64. * numpy.array([
+    [24, 10, 12, 26, 35, 47, 49, 37],
+    [ 8,  0,  2, 14, 45, 59, 61, 51],
+    [22,  6,  4, 16, 43, 57, 63, 53],
+    [30, 20, 18, 28, 33, 41, 55, 39],
+    [34, 46, 48, 36, 25, 11, 13, 27],
+    [44, 58, 60, 50,  9,  1,  3, 15],
+    [42, 56, 62, 52, 23,  7,  5, 17],
+    [32, 40, 54, 38, 31, 21, 19, 29]
+])
+
 def _bayer(image_matrix, palette_name, size):
     new_matrix = numpy.copy(image_matrix)
     cols, rows, depth = image_matrix.shape
@@ -74,7 +85,7 @@ def _cluster_dot(image_matrix, palette_name, size):
             if size == 4:
                 old_pixel += old_pixel * _clusterdot4x4map[x % 4][y % 4]
             elif size == 8:
-                pass #old_pixel += old_pixel * _bayer8x8map[x % 8][y % 8]
+                old_pixel += old_pixel * _clusterdot8x8map[x % 8][y % 8]
             else:
                 pass
             new_pixel = numpy.array(utils.closest_palette_color(old_pixel,
@@ -85,10 +96,14 @@ def _cluster_dot(image_matrix, palette_name, size):
 def cluster_dot4x4(image_matrix, palette_name):
     return _cluster_dot(image_matrix, palette_name, 4)
 
+def cluster_dot8x8(image_matrix, palette_name):
+    return _cluster_dot(image_matrix, palette_name, 8)
+
 _available_methods = {
         'bayer4x4' : bayer4x4,
         'bayer8x8' : bayer8x8,
-        'cluster4x4' : cluster_dot4x4
+        'cluster4x4' : cluster_dot4x4,
+        'cluster8x8' : cluster_dot8x8,
 }
 
 if __name__ == '__main__':
