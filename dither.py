a4515f13ae469320307def9a676de1103454a539
Alok Hota <ahota@vols.utk.edu>
2018-06-26 09:31:50 -0500

    added collage generation

--- dither.py
+++ dither.py
@@ -22,6 +22,29 @@ available_methods.update(randomized._available_methods)
 available_methods.update(error_diffusion._available_methods)
 available_methods.update(threshold._available_methods)
 
+def create_collage(image_filename):
+    image = utils.open_image(image_filename)
+    width, height = image.size
+
+    n_palettes = len(palette.available_palettes)
+    n_methods  = len(available_methods)
+
+    canvas_size = (width * n_palettes, height * n_methods)
+    canvas = Image.new('RGB', canvas_size)
+
+    image_matrix = utils.pil2numpy(image)
+
+    for p_i, p in enumerate(palette.available_palettes):
+        for m_i, m in enumerate(available_methods):
+            print 'palette {:2d}/{:2d}, method {:2d}/{:2d}'.format(p_i, n_palettes, m_i, n_methods)
+            print p, m
+            image_offset = (p_i * width, m_i * height)
+            dither_matrix = available_methods[m](image_matrix, p)
+            dither_image = utils.numpy2pil(dither_matrix)
+            canvas.paste(dither_image, image_offset)
+
+    canvas.save('collage.png')
+
 if __name__ == '__main__':
     import argparse
 
@@ -29,15 +52,20 @@ if __name__ == '__main__':
     parser.add_argument('image_filename', help='Path to an image file to dither')
     palette_help_str = 'Name of palette to use. Can be one of: ' + ', '.join(palette.available_palettes)
     method_help_str = 'Method to use. Can be one of: ' + ', '.join(available_methods)
+    all_help_str = 'Create a collage using all palettes and all dithering methods'
     parser.add_argument('-p', '--palette', type=str, default=default_palette, help=palette_help_str)
     parser.add_argument('-m', '--method', type=str, default=default_method, help=method_help_str)
+    parser.add_argument('-a', '--all', action='store_true', help=all_help_str)
     args = parser.parse_args()
 
-    image = utils.open_image(args.image_filename)
-    image_matrix = utils.pil2numpy(image)
+    if args.all:
+        create_collage(args.image_filename)
+    else:
+        image = utils.open_image(args.image_filename)
+        image_matrix = utils.pil2numpy(image)
 
-    dither_matrix = available_methods[args.method](image_matrix, args.palette)
-    dither_image = utils.numpy2pil(dither_matrix)
+        dither_matrix = available_methods[args.method](image_matrix, args.palette)
+        dither_image = utils.numpy2pil(dither_matrix)
 
-    dither_image.show()
+        dither_image.show()
 
